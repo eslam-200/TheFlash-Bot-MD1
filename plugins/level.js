0d348f615ab509dd0d91036c5f29b88b95032e2d
Escona36 <eslamelhakem18@gmail.com>
2024-09-27 12:35:55 +0300

    Update level.js

--- plugins/level.js
+++ plugins/level.js
@@ -84,19 +84,19 @@ throw `╮ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆
 ╮ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ╭ـ
 > 🧶 تحتاج الي ⌟${user.exp - min}/${xp}⌜ نقطة لي الارتفاع في المستوي 
 ╯ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ╰ـ
-> 𝐅𝐋𝐀𝐒𝐇-𝐁𝐎𝐓 © 𝐁𝐘 𝐍𝐀𝐑𝐔𝐓𝐎&𝐙𝐀𝐂𝐊
+> 𝐅𝐋𝐀𝐒𝐇-BoT © 𝐁𝐘 𝐍𝐀𝐑𝐔𝐓𝐎&𝐙𝐀𝐂𝐊
 `.trim()}
 
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 if (before !== user.level) {
 let teks = `عاش يا حب! ${conn.getName(m.sender)} المستوي: ${user.level}`
-let str = `👑 𝐅𝐋𝐀𝐒𝐇﹝⚡﹞𝐁𝐎𝐓 - 𝐍𝐀𝐑𝐔𝐓𝐎&𝐙𝐀𝐂𝐊
+let str = `👑 ESCONAR﹝⚡﹞𝐁𝐎𝐓 - ESLAM#ESCONAR
 *╭━⊰ ${name}⁩ ⊱━დ*
 *┃ الـمـسـتـوي السابق: ${before}*
 *┃ الـمـسـتـوي الحالي: ${user.level}*
 *┃ رتـبــتـك:* *‎${role}*
-> 𝐅𝐋𝐀𝐒𝐇-𝐁𝐎𝐓 © 𝐁𝐘 𝐍𝐀𝐑𝐔𝐓𝐎&𝐙𝐀𝐂𝐊
+> ESCONAR-𝐁𝐎𝐓 © 𝐁𝐘 ESCONAR
 *╰━⊰ 🎖️ مستوى جديد 🎖️ ⊱━━დ*
 
 *_لقد وصلت إلى مستوى جديد!!!_*`.trim()
