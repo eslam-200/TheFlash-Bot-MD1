07c13e47bacab1852d037a9a2c1e3340e1b8ce3e
Escona36 <eslamelhakem18@gmail.com>
2024-10-01 22:16:58 +0300

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let message = `*قــلــب اسكونر مـن جـوا 👀⚡*`;
 await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
-    conn.sendFile(m.chat, 'https://qu.ax/zfuY.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://qu.ax/rovX.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^اسكونر$/i;
