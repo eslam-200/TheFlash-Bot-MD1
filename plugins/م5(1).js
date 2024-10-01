e0f3c96d352343cf708223698b4a91af7f5ae227
Escona36 <eslamelhakem18@gmail.com>
2024-10-01 23:23:07 +0300

    Update م5(1).js

--- plugins/م5(1).js
+++ plugins/م5(1).js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isP
 try {
 let vn = './Menu2.jpg'
 let pp = imagen4
-let img = await(await fetch('https://telegra.ph/.')).buffer()
+let img = await(await fetch('https://qu.ax/nPJB.jpg.')).buffer()
 let d = new Date(new Date + 3600000)
 let locale = 'ar'
 let week = d.toLocaleDateString(locale, { weekday: 'long' })
