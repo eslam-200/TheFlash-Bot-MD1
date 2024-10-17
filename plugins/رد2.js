093f2bf95842d9bc202b0725d3a704ee23cac65d
eslam-200 <eslam2004cgg@gmail.com>
2024-10-17 12:40:43 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
-await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
+    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ❣️*';
+await conn.sendMessage(m.chat, { react: { text: '​​​​​​​​​​​​​​​🥺', key: m.key } })
     conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
 };
 
