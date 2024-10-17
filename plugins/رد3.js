c8075feedcd0d1b9809c90a9cf05fef9f05449c0
eslam-200 <eslam2004cgg@gmail.com>
2024-10-17 12:41:32 +0300

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*قــلــب فـلاش مـن جـوا 👀⚡*`;
-await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
+    let message = `*قــلــب فـلاش مـن جـوا ❣️*`;
+await conn.sendMessage(m.chat, { react: { text: '​​​​​​​​​​​​​​​​​​​😁', key: m.key } })
     conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
 };
 
